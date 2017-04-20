68945fab04cd80755862dd0efccc4694eb187896
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-04-20 19:05:12 -0400

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cuisines = Cuisine.create([{name: 'Korean'}, {name: 'Italian'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Steak'}, {name: 'Seafood'}, {name: 'Chinese'}, {name: 'French'}, {name: 'Mexican'}, {name: 'Japanese'}, {name: 'Pizzeria'}])
+
+User.create(name: 'Paul', email: 'paul@gmail.com', phone_number: '4168802823', password_digest: 'password')
+User.create(name: 'Mark', email: 'mark@gmail.com', phone_number: '4168802823', password_digest: 'password')
+User.create(name: 'John', email: 'john@gmail.com', phone_number: '4168802823', password_digest: 'password')
+User.create(name: 'Swapna', email: 'swapna@gmail.com', phone_number: '4168802823', password_digest: 'password')
+User.create(name: 'Azadeh', email: 'azadeh@gmail.com', phone_number: '4168802823', password_digest: 'password')
+User.create(name: 'Jenny', email: 'jenny@gmail.com', phone_number: '4168802823', password_digest: 'password')
+User.create(name: 'Bill', email: 'bill@gmail.com', phone_number: '4168802823', password_digest: 'password')
