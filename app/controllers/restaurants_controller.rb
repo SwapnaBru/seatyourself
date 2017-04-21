a5271b24989b839689f0ab53a9600db59cc0945b
markcyr <markcyr@msn.com>
2017-04-21 16:24:21 -0400

    added restaurant sign up page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,8 +6,26 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+        redirect_to restaurants_url
+      else
+        render :new
+      end
+  end
+
 
   def edit
   end
+
+
+private
+ def restaurant_params
+      params.require(:restaurant).permit(:name,:time_close, :time_open, :address, :city, :postal_code, :image,
+      :user_id, :cuisine_id, :capacity, :price_range)
+ end
 end
