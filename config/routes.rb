5a8d545483009f924e4c8b7e4b5bb13667f040de
Azbagheri <azadeh.bagheri7@gmail.com>
2017-04-21 15:40:10 -0400

    Adding the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'home/index'
-  resource :user, only: [:new, :create]
+  resources :users, only: [:new, :create]
   resources :reservations
   resources :restaurants
   resources :sessions, only: [:new, :create, :destroy]
