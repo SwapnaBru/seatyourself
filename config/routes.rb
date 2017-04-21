1d8c3d7c1964226ca1c49c41e852bc9186806961
Azbagheri <azadeh.bagheri7@gmail.com>
2017-04-21 14:12:29 -0400

    Adding home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
 
-
-  get 'restaurants/index'
-
-  get 'restaurants/show'
-
-  get 'restaurants/new'
-
-  get 'restaurants/edit'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  root 'home#index'
+  get 'home/index'
   resource :user, only: [:new, :create]
   resources :reservations
   resources :restaurants
